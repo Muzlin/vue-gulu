0ac73d345dd3cbb2119666f7ba6f4f727300e274
MuzLin <1030996014@qq.com>
2018-07-04 01:46:07 +0800

    创建button-group

---
+++ src/button-group.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="g-button-group">
+    <slot></slot>
+  </div>
+</template>
+<script>
+  export default {}
+</script>
+<style lang="scss">
+.g-button-group{
+  display: inline-flex;
+  > .g-button{
+    border-radius: 0;
+    &:first-child{
+      border-top-left-radius: var(--border-radius);
+      border-bottom-left-radius: var(--border-radius);
+    }
+    &:last-child{
+      border-top-right-radius: var(--border-radius);
+      border-bottom-right-radius: var(--border-radius);
+    }
+    margin-left: -1px;
+
+    &:hover{
+      border-color: red;
+      z-index: 1;
+    }
+  }
+}
+</style>
