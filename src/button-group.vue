b209f6ceb4f68b39aeea1bf9c5285d90f7a92db1
MuzLin <1030996014@qq.com>
2018-07-04 02:04:35 +0800

    解决一个bug  第一个元素 不需要margin-let:-1px

--- src/button-group.vue
+++ src/button-group.vue
@@ -5,10 +5,10 @@
 </template>
 <script>
   export default {
-    mounted(){
-      for (let node of this.$el.children){
+    mounted() {
+      for (let node of this.$el.children) {
         let name = node.nodeName
-        if(name.toLowerCase() !== 'button'){
+        if (name.toLowerCase() !== 'button') {
           console.warn(`g-button-group 的子元素必须为button 你出现了一个<${node.nodeName.toLowerCase()}>元素`)
         }
       }
@@ -16,23 +16,24 @@
   }
 </script>
 <style lang="scss">
-.g-button-group{
-  display: inline-flex;
-  > .g-button{
-    border-radius: 0;
-    &:first-child{
-      border-top-left-radius: var(--border-radius);
-      border-bottom-left-radius: var(--border-radius);
-    }
-    &:last-child{
-      border-top-right-radius: var(--border-radius);
-      border-bottom-right-radius: var(--border-radius);
-    }
-    margin-left: -1px;
-
-    &:hover{
-      z-index: 1;
+  .g-button-group {
+    display: inline-flex;
+    > .g-button {
+      border-radius: 0;
+      &:first-child {
+        border-top-left-radius: var(--border-radius);
+        border-bottom-left-radius: var(--border-radius);
+      }
+      &:last-child {
+        border-top-right-radius: var(--border-radius);
+        border-bottom-right-radius: var(--border-radius);
+      }
+      &:not(:first-child) {
+        margin-left: -1px;
+      }
+      &:hover {
+        z-index: 1;
+      }
     }
   }
-}
 </style>
