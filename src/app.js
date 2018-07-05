b47fb2fbdffe252ed11193cd38250618b99d706e
MuzLin <1030996014@qq.com>
2018-07-05 11:23:13 +0800

    使用chai.js 完成单元测试

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Button from './button'
-import Icon from './icon'
 import Button_group from './button-group'
+import Icon from './icon'
 
 Vue.component('g-button', Button)
 Vue.component('g-icon', Icon)
@@ -14,3 +14,90 @@ new Vue({
     l2: false
   }
 })
+
+
+/* 单元测试 */
+
+import chai from 'chai'
+
+const expect = chai.expect
+{
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData: {
+      icon: 'settings'
+    }
+  })
+  vm.$mount()
+  let useElement = vm.$el.querySelector('use')
+  let href = useElement.getAttribute('xlink:href')
+  expect(href).to.eq('#i-settings')
+}
+
+{
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData: {
+      icon: 'settings',
+      loading: true
+    }
+  })
+  vm.$mount()
+  let useElement = vm.$el.querySelector('use')
+  let href = useElement.getAttribute('xlink:href')
+  expect(href).to.eq('#i-loading')
+}
+
+{
+  let div = document.createElement('div')
+  document.body.appendChild(div)
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData: {
+      icon: 'settings'
+    }
+  })
+  vm.$mount(div)
+  let svg = vm.$el.querySelector('svg')
+  let {order} = window.getComputedStyle(svg)
+  expect(order).to.eq('1')
+  vm.$el.remove()
+  vm.$destroy()
+}
+
+{
+  let div = document.createElement('div')
+  document.body.appendChild(div)
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData: {
+      icon: 'settings',
+      iconPosition: 'right'
+    }
+  })
+  vm.$mount(div)
+  let svg = vm.$el.querySelector('svg')
+  let {order} = window.getComputedStyle(svg)
+  expect(order).to.eq('2')
+  vm.$el.remove()
+  vm.$destroy()
+}
+
+/* 单元测试 按钮点击事件 */
+{
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData: {
+      icon: 'settings',
+      iconPosition: 'right'
+    }
+  })
+  vm.$mount()
+  vm.$on('click',()=>{
+    expect(1).to.eq(1)
+  })
+  let button = vm.$el
+  button.click()
+  vm.$el.remove()
+  vm.$destroy()
+}
